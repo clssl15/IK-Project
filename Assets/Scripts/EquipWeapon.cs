705bdcd70616b216b3d15b92a4746acd7f4caf95
minsoo <clssl15@naver.com>
2026-02-11 23:31:00 +0900

    [ShootingScene_Lab1] 1차 IK 적용 시 발생한 어색한 움직임 수정을 위한 테스트 씬 생성

--- Assets/Scripts/EquipWeapon.cs
+++ Assets/Scripts/EquipWeapon.cs
@@ -1,11 +1,26 @@
 using UnityEngine;
 using StarterAssets;
+using UnityEngine.Animations.Rigging;
 
 public class EquipWeapon : MonoBehaviour
 {
     [Header("Current Weapon Transform Settings")]
     [SerializeField]
-    private Transform currentWeaponPos;
+    private Transform equipPos;
+    [SerializeField]
+    private Transform aimingPos;
+
+    [Header("Right Hand Target")]
+    [SerializeField]
+    private TwoBoneIKConstraint rightHandIK;
+    [SerializeField]
+    private Transform rightHandTarget;
+
+    [Header("Left Hand Target")]
+    [SerializeField]
+    private TwoBoneIKConstraint leftHandIK;
+    [SerializeField]
+    private Transform leftHandTarget;
 
     private StarterAssetsInputs _input;
     private Animator playerAnimator;
@@ -22,18 +37,24 @@ public class EquipWeapon : MonoBehaviour
     {
         if (_currentWeapon == null) return;
 
+        Transform newPos = default;
+
         if (_input.aim)
         {
             switch (_currentWeapon.Type)
             {
                 case WeaponType.pistol:
                     playerAnimator.SetBool("PistolAim", true);
+                    newPos = aimingPos;
                     break;
 
                 case WeaponType.assault:
                     playerAnimator.SetBool("AssaultAim", true);
+                    newPos = aimingPos;
                     break;
             }
+
+            leftHandIK.weight = 1f;
         }
         else if (!_input.aim)
         {
@@ -41,17 +62,26 @@ public class EquipWeapon : MonoBehaviour
             {
                 case WeaponType.pistol:
                     playerAnimator.SetBool("PistolAim", false);
+                    newPos = equipPos;
                     break;
                 case WeaponType.assault:
+                    newPos = equipPos;
                     playerAnimator.SetBool("AssaultAim", false);
                     break;
             }
+
+            leftHandIK.weight = 0f;
         }
+
+        SetWeaponPos(newPos);
+        SetIKPos();
     }
 
     private void OnTriggerStay(Collider other)
     {
         if (!_input.interact) return;
+
+        if (_input.aim) return;
 
         if (other.CompareTag("Weapon"))
         {
@@ -61,10 +91,33 @@ public class EquipWeapon : MonoBehaviour
 
     void Equip(Weapon weapon)
     {
+        if (_currentWeapon != null)
+        {
+            // 현재 장착중인 무기 떨어트리기
+            _currentWeapon.transform.parent = null;
+            _currentWeapon.transform.position = transform.position + transform.forward;
+        }
+
         _currentWeapon = weapon;
 
-        _currentWeapon.transform.parent = currentWeaponPos.transform;
-        _currentWeapon.transform.position = currentWeaponPos.position;
-        _currentWeapon.transform.rotation = currentWeaponPos.rotation;
+        rightHandIK.weight = 1f;
+
+        playerAnimator.SetBool("HasWeapon", true);
+    }
+
+    void SetWeaponPos(Transform newPos)
+    {
+        _currentWeapon.transform.SetParent(newPos.transform);
+        _currentWeapon.transform.position = newPos.position;
+        _currentWeapon.transform.rotation = newPos.rotation;
+    }
+
+    void SetIKPos()
+    {
+        rightHandTarget.position = _currentWeapon.RightHandPos.position;
+        rightHandTarget.rotation = _currentWeapon.RightHandPos.rotation;
+
+        leftHandTarget.position = _currentWeapon.LeftHandPos.position;
+        leftHandTarget.rotation = _currentWeapon.LeftHandPos.rotation;
     }
 }
--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -15,5 +15,15 @@ public class Weapon : MonoBehaviour
 {
     [SerializeField]
     private WeaponType _type;
+
+    [Header("IK Hand Position")]
+    [SerializeField]
+    private Transform _rightHandPos;
+    [SerializeField]
+    private Transform _leftHandPos;
+
+    // 프로퍼티
     public WeaponType Type => _type;
+    public Transform RightHandPos => _rightHandPos;
+    public Transform LeftHandPos => _leftHandPos;
 }
